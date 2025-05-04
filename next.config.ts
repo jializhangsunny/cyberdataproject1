aa963d59c29c5baeedda7a44c0ad015853862ada
jializhangsunny <jializhangsunny@outlook.com>
2025-05-04 17:50:46 +0100

    yml

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /**
-   * Enable static exports.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: "export",
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/cyberdataproject1",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-      unoptimized: true,
-  },
+module.exports = {
+  output: 'export',
 };
-
-export default nextConfig;
